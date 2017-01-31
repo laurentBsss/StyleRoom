dead15bfb4968d13186de56ff224caaea30c00db
laurentBsss <lbasses@alcopa-auction.fr>
2017-01-31 14:42:13 +0100

    add imagecontroller

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -17,6 +17,51 @@ angular.module('starter.controllers', [])
   };
 })
 
+
+.controller('ImageController', function($scope, $cordovaDevice, $cordovaFile, $ionicPlatform, $cordovaEmailComposer, $ionicActionSheet, ImageService, FileService) {
+ 
+  $ionicPlatform.ready(function() {
+    $scope.images = FileService.images();
+    if(!$scope.$$phase) {
+      $scope.$apply();
+    }
+   
+  });
+ 
+  $scope.urlForImage = function(imageName) {
+    var trueOrigin = cordova.file.dataDirectory + imageName;
+    return trueOrigin;
+  }
+ 
+  $scope.addMedia = function() {
+    $scope.hideSheet = $ionicActionSheet.show({
+      buttons: [
+        { text: 'Take photo' },
+        { text: 'Photo from library' }
+      ],
+      titleText: 'Add images',
+      cancelText: 'Cancel',
+      buttonClicked: function(index) {
+        $scope.addImage(index);
+      }
+    });
+  }
+ 
+  $scope.addImage = function(type) {
+    $scope.hideSheet();
+    ImageService.handleMediaDialog(type).then(function() {
+       if(!$scope.$$phase) {
+        $scope.$apply();
+       }
+    });
+  }
+  
+})
+
+
+
+
+
 .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
   $scope.chat = Chats.get($stateParams.chatId);
 })
