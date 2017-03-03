915b15b8dd8b976bcc412f3150119c39fba13bab
laurentBsss <lbasses@alcopa-auction.fr>
2017-03-03 16:49:12 +0100

    add login fb popup

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -304,6 +304,37 @@ angular.module('starter.controllers', ['ionic', 'ui.router'])
  
  }
 
+//login  facebook
+ $scope.loginFacebook = function($ionicPopup){ 
+    firebase.initializeApp(config);
+    var provider = new firebase.auth.FacebookAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+      var token = result.credential.accessToken;
+      console.log('le token',token);
+      // The signed-in user info.
+      var user = result.user;
+      // ...
+      $ionicPopup.alert({
+              title: 'login facebook reussit !',
+              template: 'social login opéééé'
+          });
+
+
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+  });
+
+ } 
+ //fin login fb
+
 })
 
 
